3b86581503c2b0a7668dfb4937694884449c2695
William Touboul <Touboul.william@gmail.com>
2021-01-04 14:05:28 +0100

    Ajout CSS et regex

--- Exercice8.js
+++ Exercice8.js
@@ -1,19 +1,25 @@
-function checkAge(){
+document.getElementById("submit").addEventListener('click', function checkAge(e){
+
+    e.preventDefault();
 
     var regex = new RegExp("^[0-9\,\. ]+$");
     let age = document.getElementById("age").value;
 
     if (!regex.test(age) || age == null) {
-        alert('PAS BON');
+        document.getElementById("target").innerHTML = "Vous n'avez pas saisi une valeure correcte.";
+    }
+    else if (age <= 0 || age >= 120 ) {
+        document.getElementById("target").innerHTML = "Veuillez saisir un age valide";
     }
     else {
+
         if(age >= 18) {
-            alert('Vous etes majeur');
+            document.getElementById("target").innerHTML = 'Vous etes majeur';
         } else if (age<18) {
-            alert ('Vous etes mineur');
+            document.getElementById("target").innerHTML ='Vous etes mineur';
         };
     }
   
-}
+})
 
     
